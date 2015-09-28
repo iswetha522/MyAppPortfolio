b1e88294d2b55bb99c821176467ea05a1ed7bd6b
iswetha522 <iswetha522@gmail.com>
2015-09-28 18:27:28 +0530

    created a Switch case for mainactivity.java created a ScrollView so that it can be viewed in landscape mode. changed the externalized the text labels.
    Signed-off-by: iswetha522 <iswetha522@gmail.com>

--- app/src/main/java/com/example/android/myappportfolio/MainActivity.java
+++ app/src/main/java/com/example/android/myappportfolio/MainActivity.java
@@ -5,6 +5,7 @@ import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
+import android.widget.Button;
 import android.widget.Toast;
 
 import static android.widget.Toast.LENGTH_LONG;
@@ -18,32 +19,34 @@ public class MainActivity extends AppCompatActivity {
     }
 
 
-    public void spotSteam(View view) {
+    public void myApp(View v) {
+        Button clickedButton = (Button) v;
+        switch (clickedButton.getId()) {
+            case R.id.spotify_button:
+                Toast.makeText(getApplicationContext(), "This Button will launch my SPOTIFY STREAMER APP!", LENGTH_LONG).show();
+                break;
 
-        Toast.makeText(getApplicationContext(), "This Button will launch my SPOTIFY STEAMER APP!", LENGTH_LONG).show();
-    }
+            case R.id.scores_button:
+                Toast.makeText(getApplicationContext(), "This Button will launch my SCORES APP!", LENGTH_LONG).show();
+                break;
 
-    public void scoreApp(View view) {
-        Toast.makeText(getApplicationContext(), "This Button will launch my SCORES APP!", LENGTH_LONG).show();
-    }
+            case R.id.lib_button:
+                Toast.makeText(getApplicationContext(), "This Button will launch my LIBRARY APP!", LENGTH_LONG).show();
+                break;
 
-    public void libApp(View view) {
+            case R.id.build_button:
+                Toast.makeText(getApplicationContext(), "This Button will launch my BUILD IT BIGGER APP!", LENGTH_LONG).show();
+                break;
 
-        Toast.makeText(getApplicationContext(), "This Button will launch my LIBRARY APP!", LENGTH_LONG).show();
-    }
-
-    public void buildBig(View view) {
-
-        Toast.makeText(getApplicationContext(), "This Button will launch my BUILD IT BIGGER APP!", LENGTH_LONG).show();
-    }
-
-    public void xyzRead(View view) {
-
-        Toast.makeText(getApplicationContext(), "This Button will launch my XYZ READER APP!", LENGTH_LONG).show();
-    }
-
-    public void capApp(View view) {
-
-        Toast.makeText(getApplicationContext(), "This Button will launch my CAPSTONE APP!", LENGTH_LONG).show();
+            case R.id.read_button:
+                Toast.makeText(getApplicationContext(), "This Button will launch my XYZ READER APP!", LENGTH_LONG).show();
+                break;
+            case R.id.cap_button:
+                Toast.makeText(getApplicationContext(), "This Button will launch my CAPSTONE APP!", LENGTH_LONG).show();
+                break;
+        }
     }
 }
+
+
+
